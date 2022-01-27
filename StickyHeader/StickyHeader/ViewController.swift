ea8bcc5cdebb48209ac19924360333c83abbffbf
HongzCloud <jihong21511@gmail.com>
2022-01-27 17:41:27 +0900

    ✨[#1] : 시도1 스크롤 뷰를 이용한 StickyHeader 구현

--- StickyHeader/StickyHeader/ViewController.swift
+++ StickyHeader/StickyHeader/ViewController.swift
@@ -7,13 +7,84 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, Stop {
+    func stop() {
+        scrollView.isScrollEnabled = true
+        tableView.isScrollEnabled = false
+    }
+    
 
+    @IBOutlet weak var tableView: UITableView!
+    
+    @IBOutlet weak var stikcyView: UIView!
+    @IBOutlet weak var scrollView: UIScrollView!
+    
+  
+    var tableViewDelegate = A()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        tableViewDelegate.stop = self
         // Do any additional setup after loading the view.
+        tableView.register(TableViewCell.self, forCellReuseIdentifier: "TableViewCell")
+        self.tableView.dataSource = self
+        
+        tableView.isScrollEnabled = false
+        scrollView.delegate = self
+        tableView.delegate = tableViewDelegate
     }
 
 
 }
 
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        30
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let cell = tableView.dequeueReusableCell(withIdentifier: "TableViewCell") as? TableViewCell else { return UITableViewCell() }
+        cell.backgroundColor = .darkGray
+        cell.textLabel?.text = "\(indexPath.row)"
+        return cell
+    }
+    
+    
+}
+
+extension ViewController: UITableViewDelegate {
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 100
+    }
+}
+
+extension ViewController: UIScrollViewDelegate {
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset)
+        print(stikcyView.bounds, "dd")
+        if scrollView.contentOffset.y == 175 {
+            scrollView.isScrollEnabled = false
+            tableView.isScrollEnabled = true
+        } else {
+           
+        }
+        
+        
+    }
+}
+
+class A: NSObject, UITableViewDelegate {
+    var stop: Stop!
+ 
+    func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        print(scrollView.contentOffset)
+        if scrollView.contentOffset.y <= 0 {
+            stop.stop()
+        }
+        
+    }
+}
+
+protocol Stop {
+    func stop()
+}
